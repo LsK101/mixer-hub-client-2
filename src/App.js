106dea1cfb591e5ff195196e754830c245d3d721
Patrick Panahon <patrick.panahon@gmail.com>
2018-07-27 20:19:01 -0400

    landing page, fixed headerbar to top of view, added top/body body padding to prevent header/footer overlaps

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import './App.css';
 import HeaderBar from './views/header-bar';
 import FooterBar from './views/footer-bar';
+import Home from './views/home';
 
 export default class App extends Component {
   render() {
@@ -12,6 +13,8 @@ export default class App extends Component {
         <main>
           <Route path="/" component={HeaderBar} />
           <Route path="/" component={FooterBar} />
+
+          <Route exact path="/" component={Home} />
         </main>
       </Router>
     );
---
+++ src/views/home.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+import './home.css';
+import CalculateGraphic from '../images/calculate-graphic.png';
+import CreateGraphic from '../images/create-graphic.png';
+import CritiqueGraphic from '../images/critique-graphic.png';
+import DiscoverGraphic from '../images/discover-graphic.png';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+
+      <div className="row">
+        <div className="col-12 home-header-text">
+          <h1>Welcome to MixerHub 2.</h1>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-6">
+          <img className="homepage-graphic" src={CreateGraphic} />
+        </div>
+        <div className="col-6 home-section-text">
+          <h2>Create</h2>
+          <p>
+            Share your favorite drink recipes
+            and experiment to make new ones.   
+          </p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-6">
+          <img className="homepage-graphic" src={CalculateGraphic} />
+        </div>
+        <div className="col-6 home-section-text">
+          <h2>Calculate</h2>
+          <p>
+            Fine tune your cocktail's ABV to
+            get that perfect balance of taste
+            and buzz.    
+          </p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-6">
+          <img className="homepage-graphic" src={DiscoverGraphic} />
+        </div>
+        <div className="col-6 home-section-text">
+          <h2>Discover</h2>
+          <p>
+            Discover recipes made by others. Be
+            adventurous and try something new!    
+          </p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-6">
+          <img className="homepage-graphic" src={CritiqueGraphic} />
+        </div>
+        <div className="col-6 home-section-text">
+          <h2>Critique</h2>
+          <p>
+            Give feedback on other's recipes and
+            receive feedback on your own.  
+          </p>
+        </div>
+      </div>
+
+      </div>
+    );
+  }
+}
